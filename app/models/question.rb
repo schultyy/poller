306b1cc41d5767d980455182e8bc307191e0ae0f
Jan Schulte <hello@unexpected-co.de>
2015-11-11 20:03:41 +0100

    check if url is unique

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,4 @@
 class Question < ActiveRecord::Base
   validates :title, presence: true
+  validates :url, uniqueness: true
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -9,4 +9,10 @@ RSpec.describe Question, type: :model do
   it "is not valid without title" do
     expect(Question.new().valid?).to_not eq(true)
   end
+
+  it "has a unique url" do
+    Question.create(title: "dies und das", url: "abc")
+
+    expect(Question.new(title: "dies und das", url: "abc").valid?).to be false
+  end
 end
